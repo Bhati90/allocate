10a39d0c5ab4a1a5f0885f3f2e60c4afd8ae2f30
Kishan bhati <bhatikaran634@gmail.com>
2026-01-06 19:15:43 +0530

    allocate frontend added with complete functionality

--- data/serializers.py
+++ data/serializers.py
@@ -189,49 +189,6 @@ class ActivityLogSerializer(serializers.ModelSerializer):
             'metadata',
         ]
 
-
-from rest_framework import serializers
-from .models import UserLocation
-from django.contrib.auth.models import User
-
-class UserLocationSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = UserLocation
-        fields = ['id', 'user', 'latitude', 'longitude', 'captured_at']
-        read_only_fields = ['captured_at']
-
-class BatchLocationSerializer(serializers.Serializer):
-    """
-    Serializer to handle the specific batch structure sent by the mobile app
-    """
-    user_id = serializers.IntegerField()
-    today_date = serializers.DateField() # Validates yyyy-MM-dd
-    locations = serializers.ListField(
-        child=serializers.DictField()
-    )
-
-    def create(self, validated_data):
-        user_id = validated_data.get('user_id')
-        locations_data = validated_data.get('locations')
-        
-        # Verify user exists
-        try:
-            user = User.objects.get(id=user_id)
-        except User.DoesNotExist:
-            raise serializers.ValidationError({"user_id": "User not found"})
-
-        # Bulk create for performance since we expect high frequency/batching
-        location_instances = [
-            UserLocation(
-                user=user,
-                latitude=loc['latitude'],
-                longitude=loc['longitude']
-            ) for loc in locations_data
-        ]
-        
-        return UserLocation.objects.bulk_create(location_instances)
-    
-    
 from rest_framework import serializers
 from .models import FCMDevice
 
