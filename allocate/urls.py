c8efdded10f66d3387f295bfda9d155b9ffdd646
Kishan bhati <bhatikaran634@gmail.com>
2026-02-15 00:10:04 +0530

    tender

--- allocate/urls.py
+++ allocate/urls.py
@@ -18,13 +18,13 @@ from django.contrib import admin
 # from django.urls import path, include
 from django.urls import path, include, re_path
 from django.conf import settings
-# from data.views import about
+
 from django.conf.urls.static import static
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('ap/', include('data.urls')),  # API must come FIRST
+  # API must come FIRST
     # path('', about, name='about'),
     path('tender/', include('tender.urls')),  # API must come FIRST
     
