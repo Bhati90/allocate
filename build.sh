5c3d0ffca4b55165942005e7b100d9db5d508f3e
Kishan bhati <bhatikaran634@gmail.com>
2026-02-16 11:11:40 +0530

    tender

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ set -o errexit
 pip install -r requirements.txt
 
 python manage.py migrate --noinput
-python manage.py loaddata tender_dump.json
+# python manage.py loaddata tender_dump.json
 
 python manage.py collectstatic --noinput
